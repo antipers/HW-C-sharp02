31c09fbadc93bbfa07f40d12a983088c78137c9a
antipers <antipers@gmail.com>
2022-07-01 01:46:04 +0300

    completed task10 and added .gitignore

---
+++ task10/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Введите трехзначное число- ");
+int Num;
+int a;
+int b;
+while (!int.TryParse(Console.ReadLine(), out Num))
+{
+    System.Console.WriteLine("Введен некорректный символ, введите трехзначное ЧИСЛО");
+}
+
+System.Console.WriteLine($"Число {Num} удовлетворяет критериям");
+
+if (Num > 999 | Num < 100)
+{
+    System.Console.WriteLine("Введен некорректное число, введите ТРЕХЗНАЧНОЕ");
+}
+else
+{System.Console.WriteLine($"Число {Num} является трехзначным, выполняется вычисление ...");
+
+ a = Num % 100;
+b = a / 10;
+System.Console.WriteLine(b);
+}
+
+
