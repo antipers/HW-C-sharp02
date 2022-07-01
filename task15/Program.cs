e3f86bc8cdd18b8e8809a420f47c7d91ae3edc80
antipers <antipers@gmail.com>
2022-07-01 03:18:17 +0300

    completed, but still need upgrade

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число от 1 до 7: ");
+int Num;
+
+while (!int.TryParse(Console.ReadLine(), out Num))
+{
+    System.Console.WriteLine("Введен некорректный символ, введите число от 1 до 7");
+}
+
+if (Num > 5 & Num < 8)
+{
+    System.Console.WriteLine("Сегодня выходной, приходите в другой день");
+
+}
+else System.Console.WriteLine("Сегодня будний день");
