03e1f7c7a698c4d34daa2aee90c56a41d1e87731
antipers <antipers@gmail.com>
2022-07-01 03:00:36 +0300

    completed

---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Введите число- ");
+int Num;
+
+while (!int.TryParse(Console.ReadLine(), out Num))
+{
+    System.Console.WriteLine("Введен некорректный символ, введите трехзначное ЧИСЛО");
+}
+
+System.Console.WriteLine($"Число {Num} удовлетворяет критериям");
+
+
+
+if (Num < 99)
+{
+    System.Console.WriteLine("Третья цифра числа отсутствует");
+}
+
+
+int a = int.Parse(Convert.ToString(Num)[2].ToString());
+System.Console.WriteLine(a);
